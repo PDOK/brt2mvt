356d2f47147d23e5ada21867dba064382a38fb1d
NieneB <niene.boeijen@gmail.com>
2017-03-07 15:39:40 +0100

    sql wegdeelvlak from Foreign data table

---
+++ create_wegdeelvlak.sql
@@ -0,0 +1,72 @@
+CREATE TABLE wegdeelvlak AS(
+	SELECT 
+		NULL AS top10_id,
+		NULL AS type_infrastructuur,
+		type_weg,
+		hoofdverkeersgebruik,
+		gescheiden_rijbaan,
+		NULL AS verhardingstype,
+		status,
+		hoogteniveau,
+		NULL AS verhardingsbreedteklasse,
+		NULL AS verhardingsbreedte,
+		aantal_rijstroken,
+		fysiek_voorkomen,
+		nederlandse_straatnaam,
+		friese_straatnaam,
+		wegnummer_a_weg,
+		wegnummer_n_weg,
+		wegnummer_e_weg,
+		wegnummer_s_weg,
+		NULL AS afritnummer,
+		NULL AS afritnaam,
+		knooppuntnaam,
+		brugnaam,
+		tunnelnaam,
+		NULL AS top50visualisatie,
+		NULL AS top100visualisatie,
+		NULL AS top25visualisatie,
+		viscode ,
+		ST_Transform(shape, 3857) AS geom ,
+		ARRAY[16,17,18,19] AS zoomlevel 
+	FROM fgdb_brt.wegdeelvlak_10_14 
+UNION
+	SELECT 
+		top10_id,
+		type_infrastructuur,
+		type_weg,
+		hoofdverkeersgebruik,
+		gescheiden_rijbaan,
+		verhardingstype,
+		status,
+		hoogteniveau,
+		verhardingsbreedteklasse,
+		verhardingsbreedte,
+		aantal_rijstroken,
+		fysiek_voorkomen,
+		nederlandse_straatnaam,
+		friese_straatnaam,
+		wegnummer_a_weg,
+		wegnummer_n_weg,
+		wegnummer_e_weg,
+		wegnummer_s_weg,
+		afritnummer,
+		afritnaam,
+		knooppuntnaam,
+		brugnaam,
+		tunnelnaam,
+		top50visualisatie,
+		top100visualisatie,
+		top25visualisatie,
+		viscode ,
+		ST_Transform(shape, 3857) AS geom ,
+		ARRAY[13,14,15] AS zoomlevel 
+	FROM fgdb_brt.wegdeelvlak_7_9
+ORDER BY zoomlevel
+);
+
+ALTER TABLE wegdeelvlak ADD COLUMN fid SERIAL PRIMARY KEY;
+CREATE INDEX  wegdeelvlak_gix ON public.wegdeelvlak USING gist (geom);
+CLUSTER wegdeelvlak USING wegdeelvlak_gix;
+
+--Wegdeelvlak_10_14 bevat geen top10_id..afritnummers visualizatie codes... 
